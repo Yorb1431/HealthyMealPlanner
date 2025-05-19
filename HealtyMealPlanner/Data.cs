2d915b1fd36968da71288a353f324343a68a9016
Yorb1431 <yorbevandermast@gmail.com>
2025-05-19 19:41:03 +0200

    fix: correct formatting and ensure proper namespace structure in Data.cs

--- HealtyMealPlanner/Data.cs
+++ HealtyMealPlanner/Data.cs
@@ -2,3 +2,13 @@
 using System;
 using System.Security.Cryptography;
 using System.Text;
+
+namespace HealthyMealPlanner
+{
+    public class Data
+    {
+        private string connectionString = "server = 127.0.0.1;" +
+            "port = 3307; " +
+            "username = root; password =;" +
+            "database = healthymealplanner;";
+        // Hash password using SHA256
