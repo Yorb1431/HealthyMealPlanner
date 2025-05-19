6b1cb7ddc29a956759ecd4debd920ba104be2ec2
Yorb1431 <yorbevandermast@gmail.com>
2025-05-19 19:33:32 +0200

    feat: implement initial structure for App.xaml.cs with necessary namespaces

--- HealtyMealPlanner/App.xaml.cs
+++ HealtyMealPlanner/App.xaml.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HealthyMealPlanner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
