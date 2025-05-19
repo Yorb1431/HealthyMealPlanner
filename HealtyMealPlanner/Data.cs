5f7183ef719ddf85b88b133fbd83b8a949894c1d
Yorb1431 <yorbevandermast@gmail.com>
2025-05-19 19:41:19 +0200

    feat: implement HashPassword method for secure password hashing

--- HealtyMealPlanner/Data.cs
+++ HealtyMealPlanner/Data.cs
@@ -12,3 +12,13 @@ namespace HealthyMealPlanner
             "username = root; password =;" +
             "database = healthymealplanner;";
         // Hash password using SHA256
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
