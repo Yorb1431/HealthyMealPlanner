ff0e387af77e1ef685befa3317b93143fa2bce89
Jonas22452 <118227848+Jonas22452@users.noreply.github.com>
2025-05-23 00:25:27 +0200

    Added ResetPasswordRequestWIndow
    added window that lets the user give the email they want to reset the password for, will redirect the person(if code is correct) to SetNewPasswordWindow( will be implemented later)

---
+++ HealtyMealPlanner/Views/Login/ResetPasswordRequestWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+
+namespace HealthyMealPlanner.Views
+{
+    public partial class ResetPasswordRequestWindow : Window
+    {
+        private string resetCode;
+        private string userEmail;
+
+        public ResetPasswordRequestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendCode_Click(object sender, RoutedEventArgs e)
+        {
+            string input = EmailTextBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Enter your email or username.");
+                return;
+            }
+
+            var data = new Data();
+            userEmail = data.GetEmail(input);
+            if (string.IsNullOrWhiteSpace(userEmail))
+            {
+                MessageBox.Show("No account found for this user.");
+                return;
+            }
+
+            resetCode = GenerateVerificationCode();
+
+            data.SendEmailCode(userEmail, resetCode, "Reset");
+
+            var verifyWindow = new Login.VerifyResetCodeWindow(resetCode, userEmail);
+            verifyWindow.Show();
+            this.Close();
+        }
+
+        private string GenerateVerificationCode()
+        {
+            Random r = new Random();
+            return r.Next(100000, 999999).ToString();
+        }
+    }
+}
