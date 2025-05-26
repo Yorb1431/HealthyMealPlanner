173f2e70e545e117e3fbfbc7cbd9f9b9b907c0b3
Jonas22452 <118227848+Jonas22452@users.noreply.github.com>
2025-05-26 14:18:55 +0200

    added selectplantype
    choose between daily or weekplan

---
+++ HealtyMealPlanner/Views/Recipes/SelectPlanTypeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace HealthyMealPlanner.Views
+{
+    public partial class SelectPlanTypeWindow : Window
+    {
+        public string SelectedType { get; private set; }
+
+        public SelectPlanTypeWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        //kies voor een weekplan 
+        private void Weekly_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedType = "Week";
+            DialogResult = true;
+            Close();
+        }
+        //kies voor een dagplan
+        private void Day_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedType = "Day";
+            DialogResult = true;
+            Close();
+        }
+    }
+}
